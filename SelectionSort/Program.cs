0f063eefd63cfc4f121d6eadfea4cab2f6a67e45
lohithachandana <chandanalohitha@gmail.com>
2022-06-12 19:18:23 +0530

    Add Selection Sort

---
+++ SelectionSort/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SelectionSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SelectionSort ss = new SelectionSort();
+            int[] unsortArray = new int[4] { 2, 10, 25, 15 };
+
+            int[] sortedArray;
+
+            sortedArray = ss.selectionSort(unsortArray);
+
+            for (int i = 0; i < sortedArray.Length; i++)
+            {
+                Console.Write(sortedArray[i]+",");
+            }
+            
+        }
+    }
+}
---
+++ SelectionSort/SelectionSort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SelectionSort
+{
+    public class SelectionSort
+    {
+        public int[] selectionSort(int[] inputArray)
+        {
+            int[] array = inputArray;
+            int loopCount = array.Length - 1;
+
+            //Selecting the max value
+            while (loopCount > 0)
+            {
+                int maxValue = array[0];
+                int maxIndex = 0;
+
+                for (int i = 0; i <= loopCount; i++)
+                {
+                    if (array[i] > maxValue)
+                    {
+                        maxValue = array[i];
+                        maxIndex = i;
+                    }
+                }
+
+                //Assigning the max value
+                array[maxIndex] = array[loopCount];
+                array[loopCount] = maxValue;
+                loopCount--;
+            }
+
+            return array;
+        }
+    }
+}
